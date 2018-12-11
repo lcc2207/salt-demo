9224b64fc5b294488b874129da4041a1aeaed04a
lcc2207 <lynn@fidmail.com>
2018-12-11 13:26:17 -0600

    minion

---
+++ minion.sh
@@ -0,0 +1,14 @@
+
+cat <<EOF >/etc/yum.repos.d/salt.repo
+[saltstack-repo]
+name=SaltStack repo for Red Hat Enterprise Linux $releasever
+baseurl=https://repo.saltstack.com/yum/redhat/$releasever/$basearch/latest
+enabled=1
+gpgcheck=1
+gpgkey=https://repo.saltstack.com/yum/redhat/$releasever/$basearch/latest/SALTSTACK-GPG-KEY.pub
+       https://repo.saltstack.com/yum/redhat/$releasever/$basearch/latest/base/RPM-GPG-KEY-CentOS-7
+EOF
+
+cat <<EOF >/etc/salt/minion
+master: $salt_master
+EOF
